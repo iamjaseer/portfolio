c6c4c617a81083394263b176e510a4a6de34f39f
iamjaseer <42886352+iamjaseer@users.noreply.github.com>
2023-09-30 17:10:44 +0530

    Update page.js

--- src/app/blog-single/page.js
+++ src/app/blog-single/page.js
@@ -1,10 +1,5 @@
 "use client";
 import React from 'react'
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { useSearchParams } from 'next/navigation'
-import Link from 'next/link'
-import Carousel from 'react-elastic-carousel'
 
 
 const BlogSingle = () => {
